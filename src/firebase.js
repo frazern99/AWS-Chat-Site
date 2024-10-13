e8183a8d763c925ca546b8f2c7ce035b7cf93398
frazern99 <frazern99@gmail.com>
2024-10-13 05:38:54 -0400

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase.initializeApp({
+    apiKey: "AIzaSyCncJ23lg6bcNEVo0GALGoknBuMEhvBacY",
+    authDomain: "chat-27b79.firebaseapp.com",
+    projectId: "chat-27b79",
+    storageBucket: "chat-27b79.appspot.com",
+    messagingSenderId: "390029485171",
+    appId: "1:390029485171:web:c2d1e8908522c6341ab606"
+  }).auth();
